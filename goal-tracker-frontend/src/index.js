026ed3e79944708de2a5b7af05414d1be025e104
SzPetra <petra.szilagyi27@gmail.com>
2023-02-08 19:40:28 +0100

    add: LandingPage to application

--- goal-tracker-frontend/src/index.js
+++ goal-tracker-frontend/src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Navbar from './uiKit/components/Navbar';
-import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MyGoalsPage from './pages/myGoalsPage/MyGoalsPage';
 import CommonGoalsPage from './pages/commonGoalsPage/CommonGoalsPage';
 import UserProfilePage from './pages/userProfilePage/UserProfilePage';
+import LandingPage from './pages/landingPage/LandingPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar />}> 
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<LandingPage/>}/> 
           <Route path="/mygoals" element={<MyGoalsPage/>} />
           <Route path="/goals" element={<CommonGoalsPage/>} />
           <Route path="/myprofile" element={<UserProfilePage/>} />
---
+++ goal-tracker-frontend/src/pages/landingPage/LandingPage.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const LandingPage = () => {
+    const [punchline, setPunchline] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try{
+				const response = await fetch('https://dad-jokes.p.rapidapi.com/random/joke', {
+					method: 'GET',
+					headers: {
+						'X-RapidAPI-Key': 'a31e9338e3mshd2df809b1e4940cp1d97bejsn3a035b596426',
+						'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+			}
+				});
+				const json = await response.json();
+				console.log(json);
+				setPunchline(json.slip.punchline);
+				console.log(punchline);
+			} catch(error) {
+				console.log("error", error);
+			}
+		}
+
+		fetchData();
+	}, []);
+
+    return (
+        <p>this is the landing page</p>
+    )
+}
+
+export default LandingPage;
