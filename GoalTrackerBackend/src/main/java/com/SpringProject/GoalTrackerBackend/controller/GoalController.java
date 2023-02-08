c47ffe6eb2d5980418f9895f274853fcdd3a41b9
SzPetra <petra.szilagyi27@gmail.com>
2023-02-08 11:49:33 +0100

    create: GoalController in project with GET and POST methods

---
+++ GoalTrackerBackend/src/main/java/com/SpringProject/GoalTrackerBackend/controller/GoalController.java
@@ -0,0 +1,31 @@
+package com.SpringProject.GoalTrackerBackend.controller;
+
+import com.SpringProject.GoalTrackerBackend.model.GoalModel;
+import com.SpringProject.GoalTrackerBackend.service.GoalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("goal")
+@RestController
+public class GoalController {
+
+    private final GoalService goalService;
+
+    @Autowired
+    public GoalController(GoalService goalService) {
+        this.goalService = goalService;
+    }
+
+    @GetMapping
+    public List<GoalModel> getAllGoals() {
+        return goalService.getAllGoals();
+    }
+
+    @PostMapping
+    @ResponseBody
+    public void addNewGoal(@RequestBody GoalModel goal) {
+        goalService.addNewGoal(goal);
+    }
+}
