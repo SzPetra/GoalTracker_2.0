23e790afe3b591f663953bc2183ffaff0c4d0d94
SzPetra <petra.szilagyi27@gmail.com>
2023-02-08 19:12:52 +0100

    add: css to navigation bar

--- goal-tracker-frontend/src/uiKit/components/Navbar.js
+++ goal-tracker-frontend/src/uiKit/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import "../assets/navbar.css";
 
 const Navbar = () => {
 
     return (
         <>
             <nav className="nav-container">
-                <Link to="/mygoals">MY goals</Link>
-                <Link to="/goals">Goals</Link>
-                <Link to="/myprofile">MY profile</Link>
+                <Link className="nav-link" to="/mygoals">MY goals</Link>
+                <Link className="nav-link" to="/goals">Goals</Link>
+                <Link className="nav-link" to="/myprofile">MY profile</Link>
             </nav>
             <Outlet/>
         </>
