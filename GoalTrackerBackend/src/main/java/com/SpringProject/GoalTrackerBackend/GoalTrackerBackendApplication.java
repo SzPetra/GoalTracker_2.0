e645d872a52b0db264a7b5ab4a2f6c01d919dfee
SzPetra <petra.szilagyi27@gmail.com>
2023-02-08 09:49:25 +0100

    create: backend for GoalTracker application

---
+++ GoalTrackerBackend/src/main/java/com/SpringProject/GoalTrackerBackend/GoalTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SpringProject.GoalTrackerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ GoalTrackerBackend/src/test/java/com/SpringProject/GoalTrackerBackend/GoalTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringProject.GoalTrackerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
